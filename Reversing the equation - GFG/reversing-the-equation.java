33f3603829f4cb54828e96b4823c53c3e05dc303
krish patel <123291666+thekrishpatel@users.noreply.github.com>
2023-07-15 10:05:28 +0530

    reversing-the-equation.java

--- Reversing the equation - GFG/reversing-the-equation.java
+++ Reversing the equation - GFG/reversing-the-equation.java
@@ -4,63 +4,45 @@
 import java.io.*;
 import java.util.*;
 
-class GFG
-{
-    public static void main(String args[])throws IOException
-    {
-        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter out=new PrintWriter(System.out);
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
         int t = Integer.parseInt(in.readLine().trim());
-        while(t-- > 0)
-        {
+        while (t-- > 0) {
             String s;
             s = in.readLine().trim();
-            
+
             Solution ob = new Solution();
-            
-            out.println(ob.reverseEqn(s));    
+
+            out.println(ob.reverseEqn(s));
         }
         out.close();
     }
 }
 // } Driver Code Ends
 
+// User function Template for Java
+class Solution {
 
-class Solution
-{
-   
-    String reverseEqn(String S)
-    {
-        // your code here
-        char s[]=S.toCharArray();
-        StringBuilder res = new StringBuilder();
-        int n = S.length();
-        int i = n - 1;
-        
-        Stack<Character> st=new Stack<>();
-            
-        // traversing the string in reverse
-        while (i >= 0)
-        {
-            if (s[i] == '+' || s[i] == '-' || s[i] == '*' || s[i] == '/')
-            {
-                while (!st.isEmpty ())
-                {
-                    res.append(st.pop());
-                }
-                res.append(s[i]);
+    String reverseEqn(String S) {
+        Stack<String> st = new Stack<>();
+        String s = "";
+        String answer = "";
+        for (int i = 0; i < S.length(); i++) {
+            if (S.charAt(i) == '+' || S.charAt(i) == '-' || S.charAt(i) == '*' || S.charAt(i) == '/') {
+                st.push(s);
+                st.push(S.charAt(i) + "");
+                s = "";
+            } else {
+                s = s + "" + S.charAt(i) + "";
             }
-            else
-                st.push(s[i]);
-            i--;
         }
-        
-            // appending the last number/variable if any
-        while (!st.isEmpty ())
-        {
-            res.append(st.pop());
+        st.push(s);
+        while (!st.isEmpty()) {
+            answer = answer + "" + st.pop();
         }
-        
-        return res.toString();
+
+        return answer;
     }
 }
