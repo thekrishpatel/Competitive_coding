fa13da0329f09fa78276ccea620796fb419b45ff
krish patel <123291666+thekrishpatel@users.noreply.github.com>
2023-09-23 13:57:00 +0530

    Time: 1 ms (99.95%), Space: 43.8 MB (89.60%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,9 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 class Solution {
-    public static int singleNumber(int[] nums) {
-        Queue<Integer> al = new LinkedList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (!al.contains(nums[i])) {
-                al.add(nums[i]);
-            } else {
-                al.remove(nums[i]);
-            }
+    public int singleNumber(int[] nums) {
+        int ans=0; //since XOR with 0 returns same number 
+        for(int i=0; i<nums.length; i++){
+            ans ^= nums[i];  // ans = (ans) XOR (array element at i) 
         }
-        return al.peek();
+        return ans;    
     }
 }
