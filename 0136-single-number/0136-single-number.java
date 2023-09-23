a0e8d8be35fe04e08afea6106379fe6aaa3b4d56
krish patel <123291666+thekrishpatel@users.noreply.github.com>
2023-09-23 13:58:11 +0530

    Time: 1 ms (99.95%), Space: 44.2 MB (74.25%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,8 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans=0; //since XOR with 0 returns same number 
-        for(int i=0; i<nums.length; i++){
-            ans ^= nums[i];  // ans = (ans) XOR (array element at i) 
+        int ans=nums[0]; 
+        for(int i=1; i<nums.length; i++){
+            ans ^= nums[i]; 
         }
         return ans;    
     }
