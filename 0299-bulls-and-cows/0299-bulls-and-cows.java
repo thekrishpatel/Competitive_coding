7ef92b46b4f6fe03443ffcec94b5da7a028d5a66
krish patel <123291666+thekrishpatel@users.noreply.github.com>
2024-07-12 21:29:08 +0530

    Time: 55 ms (5.53%), Space: 44.6 MB (5.15%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.java
+++ 0299-bulls-and-cows/0299-bulls-and-cows.java
@@ -1,34 +1,22 @@
-
-import java.util.HashMap;
-
 class Solution {
+
     public String getHint(String secret, String guess) {
-        StringBuilder first = new StringBuilder(secret);
-        StringBuilder second = new StringBuilder(guess);
-        HashMap<Character, Integer> dict = new HashMap<>();
-        int numA = 0;
-        int numB = 0;
-        for (int i = secret.length() - 1; i > -1; i--)
-        {
-            if (first.charAt(i) == second.charAt(i)) {
-                numA++;
-                first.deleteCharAt(i);
-                second.deleteCharAt(i);
-            }
-            else {
-                if (!dict.containsKey(secret.charAt(i)))
-                    dict.put(secret.charAt(i), 0);
-                dict.put(secret.charAt(i), dict.get(secret.charAt(i)) + 1);
+        int bulls = 0;
+        int cows = 0;
+        int[] numbers = new int[10];
+        for (int i = 0; i < secret.length(); i++) {
+            if (secret.charAt(i) == guess.charAt(i)) {
+                bulls++;
+            } else {
+                System.out.println(numbers[secret.charAt(i) - '0']);
+                if (numbers[secret.charAt(i) - '0']++ < 0) {
+                    cows++;
+                }
+                if (numbers[guess.charAt(i) - '0']-- > 0) {
+                    cows++;
+                }
             }
         }
-        for (int i = 0; i < second.length(); i++)
-        {
-            if(dict.containsKey(second.charAt(i)) && dict.get(second.charAt(i)) > 0)
-            {
-                numB++;
-                dict.put(second.charAt(i), dict.get(second.charAt(i)) - 1);
-            }
-        }
-        return numA + "A" + numB + "B";
+        return bulls + "A" + cows + "B";
     }
 }
