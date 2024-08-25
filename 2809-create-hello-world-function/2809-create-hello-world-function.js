0d73a8af57ef43a564e512abe83ac8fccc71be8d
krish patel <123291666+thekrishpatel@users.noreply.github.com>
2024-08-25 15:25:37 +0000

    Time: 47 ms (75.02%), Space: 48.5 MB (72.84%) - LeetHub

--- 2809-create-hello-world-function/2809-create-hello-world-function.js
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -1,13 +1,10 @@
-/**
- * @return {Function}
- */
-var createHelloWorld = function() {
-    
-    return function(...args) {
-        return 'Hello World'
-    }
-};
-
+function createHelloWorld() {
+  const greeting = "Hello World";
+  
+  return function() {
+    return greeting;
+  };
+}
 /**
  * const f = createHelloWorld();
  * f(); // "Hello World"
